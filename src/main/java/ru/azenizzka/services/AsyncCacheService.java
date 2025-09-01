ab8e5aee497b32478f148ee8f87c0d13fcc2c027
Evgeny Popov <azenizzka@gmail.com>
2025-09-01 16:46:27 +0700

    fix: fix cycle dependency

---
+++ src/main/java/ru/azenizzka/services/AsyncCacheService.java
@@ -0,0 +1,27 @@
+package ru.azenizzka.services;
+
+import java.util.concurrent.CompletableFuture;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import ru.azenizzka.utils.Day;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AsyncCacheService {
+
+  private final LessonScheduleService lessonScheduleService;
+
+  @Async("cacheWarmExecutor")
+  public CompletableFuture<Boolean> warmGroupAsync(int groupNum, Day day) {
+    try {
+      lessonScheduleService.getLessons(groupNum, day);
+      return CompletableFuture.completedFuture(true);
+    } catch (Exception e) {
+      log.warn("Failed to warm cache for group {} day {}: {}", groupNum, day, e.getMessage());
+      return CompletableFuture.completedFuture(false);
+    }
+  }
+}
--- src/main/java/ru/azenizzka/services/CacheService.java
+++ src/main/java/ru/azenizzka/services/CacheService.java
@@ -7,7 +7,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import ru.azenizzka.repositories.PersonRepository;
 import ru.azenizzka.utils.Day;
@@ -18,7 +17,7 @@ import ru.azenizzka.utils.Day;
 public class CacheService {
 
   private final PersonRepository personRepository;
-  private final LessonScheduleService lessonScheduleService;
+  private final AsyncCacheService asyncCacheService;
 
   @Autowired private final CacheService self;
 
@@ -39,7 +38,8 @@ public class CacheService {
         final int currentGroupNum = groupNum;
 
         CompletableFuture<Void> future =
-            self.warmGroupAsync(currentGroupNum, day)
+            asyncCacheService
+                .warmGroupAsync(currentGroupNum, day)
                 .thenAccept(
                     result -> {
                       if (result) {
@@ -65,16 +65,5 @@ public class CacheService {
             + failedWarmedCounter.get());
 
     return sb.toString();
-  }
-
-  @Async("cacheWarmExecutor")
-  public CompletableFuture<Boolean> warmGroupAsync(int groupNum, Day day) {
-    try {
-      lessonScheduleService.getLessons(groupNum, day);
-      return CompletableFuture.completedFuture(true);
-    } catch (Exception e) {
-      log.warn("Failed to warm cache for group {} day {}: {}", groupNum, day, e.getMessage());
-      return CompletableFuture.completedFuture(false);
-    }
   }
 }
