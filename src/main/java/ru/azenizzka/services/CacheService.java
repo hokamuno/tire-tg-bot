d6934683bd17f0b7c5312e687fce8fb533678c82
Evgeny Popov <azenizzka@gmail.com>
2025-09-01 16:05:25 +0700

    chore: add logging for warm service

--- src/main/java/ru/azenizzka/services/CacheService.java
+++ src/main/java/ru/azenizzka/services/CacheService.java
@@ -2,12 +2,14 @@ package ru.azenizzka.services;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.azenizzka.repositories.PersonRepository;
 import ru.azenizzka.utils.Day;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class CacheService {
 
@@ -25,6 +27,7 @@ public class CacheService {
     StringBuilder sb = new StringBuilder();
 
     for (Day day : days) {
+      log.info("Warm day {}", day.toString());
       sb.append("\n\nDay: ").append(day.name());
 
       for (int groupNum : groupNums) {
@@ -33,9 +36,11 @@ public class CacheService {
         try {
           lessonScheduleService.getLessons(groupNum, day);
           successfullyWarmedCounter++;
+          log.info("Successfully warmed group {}", groupNum);
           sb.append("✅");
         } catch (Exception e) {
           failedWarmedCounter++;
+          log.info("Failed warmed group {}", groupNum);
           sb.append("❌ ").append(e.getMessage());
         }
       }
